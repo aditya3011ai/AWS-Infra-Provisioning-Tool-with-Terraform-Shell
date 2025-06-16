e34708c8dc5015ee295e923d787b2d1b5fdb213c
aditya3011ai <aditya3011.1990@gmail.com>
2025-06-16 13:39:55 +0530

    updating files

--- destroy.sh
+++ destroy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 echo "WARNING: This will destroy all infrastructure provisioned by Terraform."
 read -p "Are you sure? (yes/no): " confirm
 
 if [ "$confirm" == "yes" ]; then
-  terraform destroy -var-file="terraform.tfvars"
+  terraform destroy -var-file="terraform.tfvars" -auto-approve
 else
   echo "Destroy cancelled."
 fi
